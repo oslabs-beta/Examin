7a2fea1dcefc69b97c7fdd916ad63662b56dbd8b
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-21 10:42:19 -0700

    notes from ty

--- chrome-ext/content.js
+++ chrome-ext/content.js
@@ -1,5 +1,7 @@
-console.log("Chrome Extension READY!!");
+// Runs when user navigates to localhost:3000
 
+console.log('Chrome Extension READY!!');
 
+// Use chrome extension method chrome.runtime.sendMessage()
+// to send message to chrome runtime (refer to background.js)
 chrome.runtime.sendMessage({ action: 'injectScript' });
-
