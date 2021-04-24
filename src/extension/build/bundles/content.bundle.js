ca8e0874fa428d319dd20cf33443946891532b83
Njbrush <njbrush@gmail.com>
2021-04-24 04:00:54 +0100

    Initial backend changes

--- src/extension/build/bundles/content.bundle.js
+++ src/extension/build/bundles/content.bundle.js
@@ -1,1 +1,154 @@
-console.log("Chrome Extension READY!!"),chrome.runtime.sendMessage({action:"injectScript"});
+/* -------------------------------------------------- */
+
+/*  Start of Webpack Chrome Hot Extension Middleware  */
+
+/* ================================================== */
+
+/*  This will be converted into a lodash templ., any  */
+
+/*  external argument must be provided using it       */
+
+/* -------------------------------------------------- */
+(function (chrome, window) {
+  var signals = JSON.parse('{"SIGN_CHANGE":"SIGN_CHANGE","SIGN_RELOAD":"SIGN_RELOAD","SIGN_RELOADED":"SIGN_RELOADED","SIGN_LOG":"SIGN_LOG","SIGN_CONNECT":"SIGN_CONNECT"}');
+  var config = JSON.parse('{"RECONNECT_INTERVAL":2000,"SOCKET_ERR_CODE_REF":"https://tools.ietf.org/html/rfc6455#section-7.4.1"}');
+  var reloadPage = "true" === "true";
+  var wsHost = "ws://localhost:9090";
+  var SIGN_CHANGE = signals.SIGN_CHANGE,
+      SIGN_RELOAD = signals.SIGN_RELOAD,
+      SIGN_RELOADED = signals.SIGN_RELOADED,
+      SIGN_LOG = signals.SIGN_LOG,
+      SIGN_CONNECT = signals.SIGN_CONNECT;
+  var RECONNECT_INTERVAL = config.RECONNECT_INTERVAL,
+      SOCKET_ERR_CODE_REF = config.SOCKET_ERR_CODE_REF;
+  var runtime = chrome.runtime,
+      tabs = chrome.tabs;
+  var manifest = runtime.getManifest(); // =============================== Helper functions ======================================= //
+
+  var formatter = function formatter(msg) {
+    return "[ WCER: ".concat(msg, " ]");
+  };
+
+  var logger = function logger(msg) {
+    var level = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "info";
+    return console[level](formatter(msg));
+  };
+
+  var timeFormatter = function timeFormatter(date) {
+    return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+  }; // ========================== Called only on content scripts ============================== //
+
+
+  function contentScriptWorker() {
+    runtime.sendMessage({
+      type: SIGN_CONNECT
+    }, function (msg) {
+      return console.info(msg);
+    });
+    runtime.onMessage.addListener(function (_ref) {
+      var type = _ref.type,
+          payload = _ref.payload;
+
+      switch (type) {
+        case SIGN_RELOAD:
+          logger("Detected Changes. Reloading ...");
+          reloadPage && window.location.reload();
+          break;
+
+        case SIGN_LOG:
+          console.info(payload);
+          break;
+      }
+    });
+  } // ======================== Called only on background scripts ============================= //
+
+
+  function backgroundWorker(socket) {
+    runtime.onMessage.addListener(function (action, sender, sendResponse) {
+      if (action.type === SIGN_CONNECT) {
+        sendResponse(formatter("Connected to Chrome Extension Hot Reloader"));
+      }
+    });
+    socket.addEventListener("message", function (_ref2) {
+      var data = _ref2.data;
+
+      var _JSON$parse = JSON.parse(data),
+          type = _JSON$parse.type,
+          payload = _JSON$parse.payload;
+
+      if (type === SIGN_CHANGE) {
+        tabs.query({
+          status: "complete"
+        }, function (loadedTabs) {
+          loadedTabs.forEach(function (tab) {
+            return tab.id && tabs.sendMessage(tab.id, {
+              type: SIGN_RELOAD
+            });
+          });
+          socket.send(JSON.stringify({
+            type: SIGN_RELOADED,
+            payload: formatter("".concat(timeFormatter(new Date()), " - ").concat(manifest.name, " successfully reloaded"))
+          }));
+          runtime.reload();
+        });
+      } else {
+        runtime.sendMessage({
+          type: type,
+          payload: payload
+        });
+      }
+    });
+    socket.addEventListener("close", function (_ref3) {
+      var code = _ref3.code;
+      logger("Socket connection closed. Code ".concat(code, ". See more in ").concat(SOCKET_ERR_CODE_REF), "warn");
+      var intId = setInterval(function () {
+        logger("Attempting to reconnect (tip: Check if Webpack is running)");
+        var ws = new WebSocket(wsHost);
+        ws.addEventListener("open", function () {
+          clearInterval(intId);
+          logger("Reconnected. Reloading plugin");
+          runtime.reload();
+        });
+      }, RECONNECT_INTERVAL);
+    });
+  } // ======================= Bootstraps the middleware =========================== //
+
+
+  runtime.reload ? backgroundWorker(new WebSocket(wsHost)) : contentScriptWorker();
+})(chrome, window);
+/* ----------------------------------------------- */
+
+/* End of Webpack Chrome Hot Extension Middleware  */
+
+/* ----------------------------------------------- *//*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/extension/content.ts":
+/*!**********************************!*\
+  !*** ./src/extension/content.ts ***!
+  \**********************************/
+/***/ (() => {
+
+eval("// Runs when user navigates to localhost:3000\r\nconsole.log('Chrome Extension READY!!');\r\n// Use chrome extension method chrome.runtime.sendMessage()\r\n// to send message to chrome runtime (refer to background.js)\r\nchrome.runtime.sendMessage({ action: 'injectScript' });\r\n\n\n//# sourceURL=webpack://examin/./src/extension/content.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/extension/content.ts"]();
+/******/ 	
+/******/ })()
+;
