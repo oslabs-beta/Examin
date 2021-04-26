8501730d1caae2f24eb2c595c3130b98f35ee9aa
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-26 15:22:57 -0700

    refactored stateChanges to push describe blocks into descirbeBlocksArray > > Co-authored-by: Cliff Assucnao @WizardSource Co-authored-by: Kirsten Yoon @kirstenyoon Co-authored-by: Nicholas Brush @Njbrush Co-authored-by: Ty Doan @tdoan35

--- src/backend/statechanges.ts
+++ src/backend/statechanges.ts
@@ -1,66 +1,104 @@
 // import { prevMemoizedState, currMemoizedState, MemoizedStateDiff } from './injected.js';
 
-
-
-
-// INPUT needed: 
+// INPUT needed:
 // @param prevMemoizedState : {}
 // @param currMemoizedState : {}
 // @param MemoizedStateDiff : {}
 
-// OUTPUT: '' = describe string that continually gets concatenated, until user clicks download or save 
+// OUTPUT: '' = describe string that continually gets concatenated, until user clicks download or save
 
-export default function stateChanges(currMemoizedState: object, prevMemoizedState = null, MemoizedStateDiff = null) {
-  //---------------------------------------------------------------------------------------
-  // console.log('in statechanges')  
-  // let testType = 'addTest'
-  // let testMessage = `
-  //   // Initial describe statement for default initialized state
-  //   describe('default state', () => {
-  //     it('should return a default state when given an undefined input', () => {
-  //       // expect(currMemoizedState[0]).toEqual({});
-  //       // expect(currMemoizedState[1]).toEqual({});
-  //       // expect(currMemoizedState).toEqual([{},{}]);
-  //     });
-  //   });
-  // `;
-  // let testOverall = { type: testType, message: testMessage }
-  // let testOverall = { type: testType }
-  // window.postMessage(testOverall,'*')
-  //---------------------------------------------------------------------------------------
-  
-  // chrome.runtime.sendMessage('kjnelkhbfmfpbpjncbhdpnemfpibomio',{ action: 'addTest' });
-  let describeBlock: string = '';
-  
-  // Conditional (on initial): check if prevMemoizedState and MemoizedStateDiff is null
-  describeBlock = `
-    // Initial describe statement for default initialized state
-    describe('default state', () => {
-      it('should return a default state when given an undefined input', () => {
-        // expect(currMemoizedState[0]).toEqual({});
-        // expect(currMemoizedState[1]).toEqual({});
-        // expect(currMemoizedState).toEqual([{},{}]);
+// stateChanges accepts 1 required parameter and 4 default parameters
+// firstInvocation tracks if it is the first invocation of stateChanges()
+// describeBlockArray contains array of describe block strings
+export default function stateChanges(
+	currMemoizedState: object,
+	prevMemoizedState = null,
+	memoizedStateDiff = null,
+	firstInvocation: boolean = true,
+	describeBlockArray: Array<string> = []
+) {
+	//---------------------------------------------------------------------------------------
+	// console.log('in statechanges')
+	// let testType = 'addTest'
+	// let testMessage = `
+	//   // Initial describe statement for default initialized state
+	//   describe('default state', () => {
+	//     it('should return a default state when given an undefined input', () => {
+	//       // expect(currMemoizedState[0]).toEqual({});
+	//       // expect(currMemoizedState[1]).toEqual({});
+	//       // expect(currMemoizedState).toEqual([{},{}]);
+	//     });
+	//   });
+	// `;
+	// let testOverall = { type: testType, message: testMessage }
+	// let testOverall = { type: testType }
+	// window.postMessage(testOverall,'*')
+	//---------------------------------------------------------------------------------------
+
+	// chrome.runtime.sendMessage('kjnelkhbfmfpbpjncbhdpnemfpibomio',{ action: 'addTest' });
+	// let describeBlock: string = '';
+	// const describeBlockArray: Array<string> = [];
+
+	// On first invocation of stateChanges, generate test for default initalized state. reassign firstInvocation to false
+	if (firstInvocation === true) {
+		// add test for default state to describeBlockArray
+		describeBlockArray.push(`
+      // Initial describe statement for default initialized state
+      describe('default state', () => {
+        it('should return a default state when given an undefined input', () => {
+          // expect(currMemoizedState[0]).toEqual({});
+          // expect(currMemoizedState[1]).toEqual({});
+          // expect(currMemoizedState).toEqual([{},{}]);
+        });
       });
-    });
-  `;
-  
-  // Else (not-initial)
-  describeBlock += `
-    // Added a todo!
-    describe('state changed!', () => {
-      // Added a first todo!
-      it('should useStateHook variable where component changed', () => {
-        // expect(prevMemoizedState).toNotEqual(currMemoizedState);
-        // expect(currMemoizedState).toStrictlyEqual([{},{},{text: 'test1', complete: 'false'}]);
-        // expect(MemoizedStateDiff).toStrictlyEqual([{text: 'test1', complete: 'false'}])
+    `);
+		firstInvocation = false;
+		// console.log('firstInvocation Expect FALSE:', firstInvocation);
+
+		// Else if not first invocation of stateChanges, ...
+	} else {
+		// add state changed test to describeBlockArray
+		//non-initial
+		describeBlockArray.push(`
+      // Added a todo!
+      describe('state changed!', () => {
+        // Added a first todo!
+        it('should useStateHook variable where component changed', () => {
+          // expect(prevMemoizedState).toNotEqual(currMemoizedState);
+          // expect(currMemoizedState).toStrictlyEqual([{},{},{text: 'test1', complete: 'false'}]);
+          // expect(MemoizedStateDiff).toStrictlyEqual([{text: 'test1', complete: 'false'}])
+        });
       });
-    });
-    `;
+    //   `);
+	}
+	console.log('describeBlockArray:', describeBlockArray);
+	return describeBlockArray;
 
-  return describeBlock;
+	// Conditional (on initial): check if prevMemoizedState and MemoizedStateDiff is null
+	// describeBlock = `
+	//   // Initial describe statement for default initialized state
+	//   describe('default state', () => {
+	//     it('should return a default state when given an undefined input', () => {
+	//       // expect(currMemoizedState[0]).toEqual({});
+	//       // expect(currMemoizedState[1]).toEqual({});
+	//       // expect(currMemoizedState).toEqual([{},{}]);
+	//     });
+	//   });
+	// `;
 
+	// // Else (not-initial)
+	// describeBlock += `
+	//   // Added a todo!
+	//   describe('state changed!', () => {
+	//     // Added a first todo!
+	//     it('should useStateHook variable where component changed', () => {
+	//       // expect(prevMemoizedState).toNotEqual(currMemoizedState);
+	//       // expect(currMemoizedState).toStrictlyEqual([{},{},{text: 'test1', complete: 'false'}]);
+	//       // expect(MemoizedStateDiff).toStrictlyEqual([{text: 'test1', complete: 'false'}])
+	//     });
+	//   });
+	// //   `;
 }
-
 
 // describe('TODOS state changes', () => {
 
